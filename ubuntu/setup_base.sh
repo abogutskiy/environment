f9778db18fc5bfb2b21e7bc2161982862b534fd8
abogutksiy <bogutskiy.alexandr@gmail.com>
2024-08-26 06:21:48 +0200

    Add tools, don't ask confimation for docker install

--- ubuntu/setup_base.sh
+++ ubuntu/setup_base.sh
@@ -29,6 +29,8 @@ sudo apt update \
         vim \
         curl \
         wget \
+        gnupg \
+        openssl \
         pass \
         python3 \
         python3-pip \
@@ -46,7 +48,7 @@ sudo chsh -s $(which zsh)
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update \
-    && sudo apt-get install docker-ce docker-ce-cli containerd.io
+    && sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
